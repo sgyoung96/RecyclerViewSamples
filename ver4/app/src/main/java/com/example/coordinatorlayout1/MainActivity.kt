a8a91513ed51b23d3cae99e2793a76f046c09387
송가영 <sgyoung96@naver.com>
2021-05-24 18:10:45 +0900

    액션바 수정

--- ver4/app/src/main/java/com/example/coordinatorlayout1/MainActivity.kt
+++ ver4/app/src/main/java/com/example/coordinatorlayout1/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        setSupportActionBar(toolbar) // ------------> 여기서 터진다.
+        setSupportActionBar(toolbar) // ------------> 여기서 터진다.
 
         initRecycler()
     }
