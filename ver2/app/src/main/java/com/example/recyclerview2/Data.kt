48ec9396c2efc0d6aa8d7cf59b917263b9bfe748
송가영 <sgyoung96@naver.com>
2021-05-24 10:24:05 +0900

    RecyclerView2 소스 완성

---
+++ ver2/app/src/main/java/com/example/recyclerview2/Data.kt
@@ -0,0 +1,8 @@
+package com.example.recyclerview2
+
+import java.io.Serializable
+
+data class Data(
+    val title : String,
+    val date : String
+) : Serializable
---
+++ ver2/app/src/main/java/com/example/recyclerview2/FirstActivity.kt
@@ -0,0 +1,19 @@
+package com.example.recyclerview2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_first.*
+
+class FirstActivity : AppCompatActivity() {
+
+    lateinit var datas: Data
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first)
+
+        datas = intent.getSerializableExtra("data") as Data
+
+        tvFirstViewText.text = datas.title
+    }
+}
